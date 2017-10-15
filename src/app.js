6312f43474212c75dc053d266b9dfb77deb9ee64
Cernan Bernardo <cernanb@gmail.com>
2017-10-15 09:12:15 -0700

    Add register endpoint

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => res.send({ message: 'Hello World!!!' }))
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
+  })
+})
 
 app.listen(process.env.PORT || 8081)
